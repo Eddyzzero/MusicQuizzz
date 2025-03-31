b00e9e0c91c9ad76f8a37fcec2e8e9a590c22de2
Jhon Florez <jhon.florezr@gmail.com>
2025-03-31 20:34:49 +0200

    components checking ok

--- src/router/router.js
+++ src/router/router.js
@@ -6,11 +6,7 @@ import QuizView from "@/views/QuizzView.vue";
 import ResultView from "@/views/ResultView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import QuizBlindTest from "@/components/QuizBlindTest.vue";
-<<<<<<< HEAD
 import Timer from "@/components/Timer.vue";
-=======
-import Result from "@/components/Result.vue";
->>>>>>> 2f936c1f1449497087c8ece4353bd4ee4c775c61
 
 const routes = [
   {
@@ -40,14 +36,11 @@ const routes = [
     name: "NotFound",
     component: NotFoundView,
   },
-<<<<<<< HEAD
   {
     path: "/timer",
     name: "Timer",
     component: Timer,
   },
-=======
->>>>>>> 2f936c1f1449497087c8ece4353bd4ee4c775c61
 ];
 
 const router = createRouter({

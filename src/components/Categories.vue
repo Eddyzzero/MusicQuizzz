a43b79cafb0236d7ccf549d3bc6efdc83cd15803
A.Lejeune <acelyalejeune@gmail.com>
2025-04-01 20:09:47 +0200

    home page css

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <h2>Choisissez une catégorie</h2>
+    <h2 class="category-title">Choisissez une catégorie</h2>
     <p v-if="loading">Chargement des catégories...</p>
     <p v-if="error">{{ error }}</p>
     <div v-else>
-      <router-link v-for="category in activeCategories" :key="category.id" :to="getRouteForCategory(category)">
-        <Button size="small" customClass="secondary">
-          {{ category.title }} ({{ category.questions_count }} questions)
-        </Button>
+      <router-link
+        v-for="category in activeCategories"
+        :key="category.id"
+        :to="getRouteForCategory(category)"
+      >
+        <div class="category-button">
+          <Button size="small" customClass="secondary">
+            {{ category.title }} ({{ category.questions_count }} questions)
+          </Button>
+        </div>
       </router-link>
     </div>
   </div>
@@ -54,7 +60,7 @@ export default {
       return category.title === "Blind test"
         ? `/blind-test/${category.id}`
         : `/quizz/${category.id}`;
-    }
+    },
   },
 };
 </script>
@@ -64,7 +70,39 @@ router-link {
   text-decoration: none;
 }
 
-button {
-  margin: 0.5rem;
+.category-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid var(--light-grey);
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, border-color 0.3s ease;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    font-size: 0.8rem;
+    flex-direction: column;
+  }
+}
+
+.category-title {
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+  color: #fff;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  font-family: var(--font-family);
+  font-weight: 700;
+  position: absolute;
+  top: 78%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  @media (max-width: 768px) {
+    width: 100%;
+    font-size: 1.2rem;
+  }
 }
 </style>
